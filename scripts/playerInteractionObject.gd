4e7babee4289b69536c7fe361a67cce60ae0c921
iancolaura <iancovesculaura@gmail.com>
2025-11-08 20:02:13 +0100

    outline around objects works
    when player enters area, object becomes outlined

--- scripts/playerInteractionObject.gd
+++ scripts/playerInteractionObject.gd
@@ -4,8 +4,8 @@ const SPEED = 150.0
 
 func _physics_process(delta):
 	var input_vector = Vector2(
-	Input.get_action_strength("move_left") - Input.get_action_strength("move_right"),
-	Input.get_action_strength("move_up") - Input.get_action_strength("move_down") 
+	Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+	Input.get_action_strength("move_down") - Input.get_action_strength("move_up") 
 )
 
 	if input_vector.length() > 0:
