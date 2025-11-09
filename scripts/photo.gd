62afac0856e3f2cebcb8be489b8eca7d55bdca36
iancolaura <iancovesculaura@gmail.com>
2025-11-09 21:50:18 +0100

    photo reacts to "E" and player
    increases in size, flips, resets

---
+++ scripts/photo.gd
@@ -0,0 +1,70 @@
+extends "res://scripts/interactable_object.gd"
+
+@onready var front: Sprite2D = $Sprite_Front
+@onready var back: Sprite2D = $Sprite_Back
+
+var is_front := true
+var is_zoomed := false
+var start_scale: Vector2
+var photo_active := false   
+
+func _ready():
+	start_scale = scale
+
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		if not is_zoomed:
+			_on_interact()
+		else:
+			_on_interact()
+
+func _on_interact():
+	if not is_zoomed:
+		is_zoomed = true
+		photo_active = true
+		_hide_outline()
+		_zoom_in()
+	elif is_zoomed and is_front:
+		_flip_photo()
+		_hide_outline()
+	else:
+		_reset_photo()
+
+func _zoom_in():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", start_scale * 2, 0.2)
+
+func _flip_photo():
+	var tween = create_tween()
+	tween.tween_property(self, "scale:x", 0, 0.15)
+	tween.tween_callback(Callable(self, "_toggle_sides"))
+	tween.tween_property(self, "scale:x", start_scale.x * 2, 0.15)
+
+func _toggle_sides():
+	is_front = !is_front
+	front.visible = is_front
+	back.visible = !is_front
+
+func _reset_photo():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", start_scale, 0.2)
+	tween.tween_callback(Callable(self, "_reset_state"))
+
+func _reset_state():
+	is_zoomed = false
+	is_front = true
+	front.visible = true
+	back.visible = false
+	photo_active = false
+	if not photo_active and get_node("Area2D").has_overlapping_bodies():
+		outline.visible = true
+
+func _show_outline():
+	if photo_active:
+		outline.visible = false
+	else:
+		if get_node("Area2D").has_overlapping_bodies():
+			outline.visible = true
+
+func _hide_outline():
+	outline.visible = false
