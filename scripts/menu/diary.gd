0b2b573866ba1c2c6d2ad495606a9431291f1395
katrinagureev <katrin.agureev@mni.thm.de>
2025-11-16 16:14:21 +0100

    tested diary

--- scripts/menu/diary.gd
+++ scripts/menu/diary.gd
@@ -1,10 +1,36 @@
 extends Control
 
+var all_entries: Dictionary = {}
+var unlocked_entries: Array[String] = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_diary_data()
+	unlock_entry("entry_1")
+	unlock_entry("entry_2")
+	print("Entries loaded:", all_entries)
+	print("Unlocked:", unlocked_entries)
+	
 
+func load_diary_data():
+	var file = FileAccess.open("res://diaryText/diary.json", FileAccess.READ)
+	var parsed = JSON.parse_string(file.get_as_text())
+	if parsed is Dictionary and parsed.has("entries"):
+		all_entries = parsed["entries"]
+	else: 
+		print("Fehler beim Laden")
+
+func unlock_entry(id: String):
+	if not unlocked_entries.has(id) and all_entries.has(id):
+		unlocked_entries.append(id)
+
+func is_unlocked(id: String) -> bool:
+	return unlocked_entries.has(id)
+
+func get_header(id: String) -> String:
+	return all_entries[id].get("header", "")
+
+func get_pages(id: String) -> Array:
+	return all_entries[id].get("pages", [])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/menu/diary_menu.gd
+++ scripts/menu/diary_menu.gd
@@ -1,9 +1,9 @@
 extends Control
-var current_entry_id = ""
+#var current_entry_id = ""
 var current_page = 0
-var pages: Array = []
-var entry_header = ""
-
+#var pages: Array = []
+#var entry_header = ""
+var pages = []
 @onready var header_left: Label = $Panel/Header
 @onready var text_left: Label = $Panel/Text
 
@@ -13,22 +13,22 @@ var entry_header = ""
 @onready var prev_button: Button = $Panel/PrevButton
 @onready var next_button: Button = $Panel2/NextButton
 
-func open_entry(id: String):
-	if not Diary.is_unlocked(id):
-		return
-	current_entry_id = id
-	pages = Diary.get_pages(id)
-	entry_header = Diary.get_header(id)
+func _ready() -> void:
+	pages = build_all_pages()
+	update_pages()
+
+func open_entries():
+	pages = build_all_pages()
 	current_page = 0
 	update_pages()
 
 func update_pages():
-	header_left.text = entry_header
-	text_left.text = pages[current_page]
+	header_left.text = pages[current_page].header
+	text_left.text = pages[current_page].text
 	
 	if current_page + 1 < pages.size():
-		header_right.text = entry_header
-		text_right.text = pages[current_page +1]
+		header_right.text = pages[current_page + 1].header
+		text_right.text = pages[current_page +1].text
 		$Panel2.show()
 	else:
 		header_right.text = ""
@@ -38,10 +38,20 @@ func update_pages():
 	next_button.disabled = current_page + 2 > pages.size()
 
 
+func build_all_pages():
+	var combined_pages = []
+	
+	for id in Diary.unlocked_entries:
+		var header = Diary.get_header(id)
+		var pages = Diary.get_pages(id)
+		for p in pages:
+			combined_pages.append({
+			"header": header,
+			"text": p
+		})
+	return combined_pages
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -56,4 +66,6 @@ func _on_prev_button_pressed() -> void:
 
 
 func _on_next_button_pressed() -> void:
-	pass # Replace with function body.
+	if current_page + 2 < pages.size():
+		current_page += 2
+		update_pages()
