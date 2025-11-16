401e6e6e1c06cd9664c7f0119c32586b01433231
iancolaura <iancovesculaura@gmail.com>
2025-11-16 20:45:49 +0100

    picture size when zoomed adjusted

--- scripts/objects/photo.gd
+++ scripts/objects/photo.gd
@@ -31,8 +31,9 @@ func _zoom_in():
 	outline.visible = false
 	outline_locked = true
 
+
 	var t = create_tween()
-	t.tween_property(self, "scale", start_scale * 2, 0.2)
+	t.tween_property(self, "scale", start_scale * 15, 0.2)
 
 
 func _flip_photo():
