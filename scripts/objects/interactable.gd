a8d3c0a3a246cbe157c540ff1f779552516f9a2c
iancolaura <iancovesculaura@gmail.com>
2025-11-16 23:09:09 +0100

    started on popup "press E"

--- scripts/objects/interactable.gd
+++ scripts/objects/interactable.gd
@@ -7,24 +7,35 @@ class_name Interactable
 var player_in_area := false
 var outline_locked := false
 
+var e_popup  # UI-Anzeige für "E"
+
 func _ready():
 	outline.visible = false
 	area.body_entered.connect(_on_enter)
 	area.body_exited.connect(_on_exit)
+
+	# E-Icon laden
+	var popup_scene = load("res://scenes/Menues/Press_E.tscn")
+	e_popup = popup_scene.instantiate()
+	add_child(e_popup)
+	e_popup.visible = false
+	# optional: Popup über Objekt platzieren
+	e_popup.position = Vector2(0, -20)
 
 func _on_enter(body):
 	if body.is_in_group("player"):
 		player_in_area = true
 		if not outline_locked:
 			outline.visible = true
+		e_popup.visible = true
 
 func _on_exit(body):
 	if body.is_in_group("player"):
 		player_in_area = false
 		outline.visible = false
+		e_popup.visible = false
 
 func interact():
-	# default fallback – darf überschrieben werden!
 	print(name, " interacted but has no special interact() function!")
 
 func _process(delta):
