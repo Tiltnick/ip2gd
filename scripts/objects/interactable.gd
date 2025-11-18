62feea99e07c1880cd927d05b1de0d13aaaefee1
iancolaura <iancovesculaura@gmail.com>
2025-11-18 03:23:49 +0100

    popup positioning and interaction works for photo

--- scripts/objects/interactable.gd
+++ scripts/objects/interactable.gd
@@ -1,40 +1,49 @@
 extends Node2D
 class_name Interactable
 
+# Nodes
 @onready var outline := $Outline
 @onready var area := $Area2D
+@export var e_popup_path: NodePath
+var e_popup: Control
 
+# State
 var player_in_area := false
 var outline_locked := false
 
-var e_popup  # UI-Anzeige für "E"
-
 func _ready():
+	# Outline initial unsichtbar
 	outline.visible = false
+
+	# Area2D Signale verbinden
 	area.body_entered.connect(_on_enter)
 	area.body_exited.connect(_on_exit)
 
-	# E-Icon laden
-	var popup_scene = load("res://scenes/Menues/Press_E.tscn")
-	e_popup = popup_scene.instantiate()
-	add_child(e_popup)
-	e_popup.visible = false
-	# optional: Popup über Objekt platzieren
-	e_popup.position = Vector2(0, -20)
+	# E-Popup Referenz holen, falls gesetzt
+	if e_popup_path != null:
+		e_popup = get_node(e_popup_path)
+		if e_popup:
+			e_popup.visible = false
+			# Position relativ zum Objekt, z.B. über dem Mittelpunkt
+			e_popup.position = Vector2(-10, -55)
 
 func _on_enter(body):
 	if body.is_in_group("player"):
+		print("Player entered Diary Area")
 		player_in_area = true
 		if not outline_locked:
 			outline.visible = true
-		e_popup.visible = true
+		if e_popup:
+			e_popup.visible = true
 
 func _on_exit(body):
 	if body.is_in_group("player"):
 		player_in_area = false
 		outline.visible = false
-		e_popup.visible = false
+		if e_popup:
+			e_popup.visible = false
 
+# Kann von speziellen Objekten überschrieben werden
 func interact():
 	print(name, " interacted but has no special interact() function!")
 
