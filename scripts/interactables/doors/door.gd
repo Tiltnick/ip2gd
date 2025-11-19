085ec7a81621a86c3af0242fd28d2aa2f3cd8439
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-11-19 19:49:18 +0100

    created door scene put door into spaceship.tscn

--- scripts/interactables/doors/door.gd
+++ scripts/interactables/doors/door.gd
@@ -1,11 +1,17 @@
-extends Node
+extends Interactable
 
+@export_file("*.tscn") var target_scene_path: String = ""
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func interact() -> void:
+	if target_scene_path == "":
+		print("Keine Zielszene gesetzt für:", name)
+		return
+	
+	# Spieler finden
+	var player = get_tree().get_first_node_in_group("player")
+	if not player:
+		print("Kein Spieler im Baum gefunden!")
+		return
+	
+	# Szene wechseln – GODOT 4 Version
+	get_tree().change_scene_to_file(target_scene_path)
