1dc65b4e13636edaa9df275e7e2dc2ccab7453be
katrinagureev <katrin.agureev@mni.thm.de>
2025-11-21 12:37:09 +0100

    menu button
    only visible when diary collected

--- scripts/menu/GameMenu/menu_button.gd
+++ scripts/menu/GameMenu/menu_button.gd
@@ -10,20 +10,19 @@ var hidden_in_scenes = [
 var _last_path = ""
 
 func update_visibility(path: String):
-	#TODO proving if diary is in inventory 
 	if path in hidden_in_scenes:
 		GlobalMenuButton.hide()
-	else:
+	#checking if diary is collected 
+	elif GameState.puzzle_state.has("spaceship_diary"):
 		GlobalMenuButton.show()
 
 
 func _process(delta: float) -> void:
-	#scene culd be in between two scnenes null, while switching to new scene
 	var scene = get_tree().current_scene
+	var path = scene.scene_file_path
+		#scene could be in between two scnenes null, while switching to new scene -> could crash
 	if scene == null:
 		return
-	
-	var path = scene.scene_file_path
 	if path != _last_path:
 		_last_path = path
 		update_visibility(path)
