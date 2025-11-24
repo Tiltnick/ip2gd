77288379a1142a403fd67b1e15dfd9e6d73a2679
DeNaice <dennis.krs@hotmail.de>
2025-11-24 17:35:21 +0100

    Charakter Hex implemented and Dialog
    Hex now has his own dialog

--- scripts/state_machine_npc/npc.gd
+++ scripts/state_machine_npc/npc.gd
@@ -36,7 +36,7 @@ func _on_body_entered(body):
 		if e_popup_node:
 			e_popup_node.visible = true
 			# Das reicht um einen Dialog zu starten :) 
-			DialogManager.start_dialog("res://dialog/oris_mr_blob.json")
+			DialogManager.start_dialog("res://dialog/first_level/oris_hex.json")
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
