a2df521432907902994b8e2708ef75d35685cdbd
katrinagureev <katrin.agureev@mni.thm.de>
2025-12-07 14:20:49 +0100

    collision of slots

--- scripts/riddles/stonepuzzle/puzzlemanager.gd
+++ scripts/riddles/stonepuzzle/puzzlemanager.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var cells: Node2D = $Cells
+@onready var cell_scene = preload("res://scenes/Riddles/stonepuzzle/cell.tscn")
+#@onready var pieces: Node2D = $Pieces
+@onready var piece_scene = preload("res://scenes/Riddles/stonepuzzle/pieces.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	init_game()
 
+func init_game():
+	draw_cells()
+
+func draw_cells():
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
