bb83bc50bf636c5f5bb6a183af82405758bf8a6b
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-12-07 12:27:47 +0100

    zwischenstand

---
+++ scripts/interactables/objects/note.gd
@@ -0,0 +1,18 @@
+extends SaveableItem
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	save_id = "cave_note"
+	super._ready()
+
+
+func interact() -> void:
+	# falls aus irgendeinem Grund schon eingesammelt → nichts tun
+	if GameState.puzzle_state.get(save_id, false):
+		
+		return
+	print("Buch eingesammelt!")
+	mark_collected()
+	
+	queue_free()
