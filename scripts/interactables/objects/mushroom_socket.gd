fece382141448d02360f188bbcc485402ce123f2
katrinagureev <katrin.agureev@mni.thm.de>
2025-12-14 11:53:40 +0100

    added mushroom sprites

--- scripts/interactables/objects/mushroom_socket.gd
+++ scripts/interactables/objects/mushroom_socket.gd
@@ -1,9 +1,13 @@
 extends Interactable
-@onready var puzzle: CanvasLayer = $"../Puzzle"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
+
 func interact():
-	puzzle.open_puzzle()
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
