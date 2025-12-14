8614b378721c6150bcd8433ebc0eddea6727c0d7
Yannick <yannickkissel@web.de>
2025-12-14 16:51:23 +0100

    Übergänge szenen

--- scripts/scene_manager/outside_3.gd
+++ scripts/scene_manager/outside_3.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+func _ready() -> void:
+	pass
 
 func configure_camera(cam: Camera2D) -> void:
 	# Cam Limits
-	cam.limit_left = -755
-	cam.limit_right = -1
-	cam.limit_top = -700
+	cam.limit_left = 35
+	cam.limit_right = 800
+	cam.limit_top = -735
 	cam.limit_bottom = 300
+	
