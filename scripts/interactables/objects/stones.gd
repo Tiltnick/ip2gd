545d1d278a9e9371952d657b57210c94381c4099
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-12-15 17:48:17 +0100

    stones

---
+++ scripts/interactables/objects/stones.gd
@@ -0,0 +1,21 @@
+extends Interactable
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func interact():
+	# if schaufel not in inventory = false 
+		# open dialog: "oh, um hier zu graben brauche ich bestimmt ein tool"
+	
+	#remove sprite
+	
+	#function check if schaufel in inventory
+	
+	pass
