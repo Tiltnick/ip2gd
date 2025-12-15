6491693a0a69f94cd3dcbb7189d91b8432eb83ee
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-12-15 18:57:47 +0100

    zwischenstand

--- scripts/interactables/objects/stones.gd
+++ scripts/interactables/objects/stones.gd
@@ -1,21 +1,30 @@
 extends Interactable
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export_file("*.tscn") 
+var replace_object_scene: String = ""
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func interact() -> void:
+	if is_in_group("stones"):
+		if hotbarglobal.inventory_items.has("shovel"):
+			remove_stones()
+		else:
+			DialogManager.start_dialog("res://dialog/outside2/no_shovel.json")
+			
+	elif not is_in_group("stones"):
+		remove_stones()
 
-func interact():
-	# if schaufel not in inventory = false 
-		# open dialog: "oh, um hier zu graben brauche ich bestimmt ein tool"
-	
-	#remove sprite
-	
-	#function check if schaufel in inventory
-	
-	pass
+func remove_stones():
+	if replace_object_scene != "":
+		var new_scene = load(replace_object_scene).instantiate()
+		
+		# Position und Parent des aktuellen Objekts übernehmen
+		var parent = get_parent()
+		new_scene.global_position = global_position
+		parent.add_child(new_scene)
+		
+		# aktuelles Objekt entfernen
+		queue_free()
+	else:
+		print("Keine Ersatzszene angegeben!")
+		queue_free()  # Optional: entferne einfach die Steine, wenn keine Szene gesetzt ist
