479235ab3653477695e113a19735b9227a7f71cd
iancolaura <iancovesculaura@gmail.com>
2025-12-16 04:09:17 +0100

    dialogue is triggered

--- scripts/interactables/objects/sams_spaceship.gd
+++ scripts/interactables/objects/sams_spaceship.gd
@@ -9,6 +9,15 @@ func _ready() -> void:
 	item_name_en = "Fluxomat"
 	super._ready()
 
+func interact() -> void:
+	# In der Welt: direkt einsammeln, kein Zoom
+	if not spawned_from_hotbar:
+		_store_in_hotbar()
+		_on_interacted()
+		return
+
+	# Aus der Hotbar: normale Zoom-Logik
+	super.interact()
 
 func _on_interacted() -> void:
 	DialogManager.start_dialog("res://dialog/outside2/find_fluxomat.json")
