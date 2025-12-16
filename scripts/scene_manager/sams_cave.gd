6db9ce3d1d257875f1ab494035894146e2f62fd8
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-12-16 20:09:21 +0100

    cave

--- scripts/scene_manager/sams_cave.gd
+++ scripts/scene_manager/sams_cave.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
+var item: FlashlightItem
+
+@onready var field_of_view: PointLight2D = $Field_of_view
+
 func _ready() -> void:
-		if not GameState.puzzle_state.get("flashlight", true):
-			DialogManager.start_dialog("res://dialog/innerMonologue/entering_sams_cave.json")
-		else:
-			pass
-			
+	if hotbarglobal.inventory_items.has("flashlight"):
+		GameState.puzzle_state[item.save_id] = true
+		field_of_view.visible = true
+	else:
+		DialogManager.start_dialog("res://dialog/innerMonologue/entering_cave.json")
+		await DialogManager.dialog_finished
+		exit_scene()
+
+func exit_scene() -> void:
+	SceneManager.goto_scene("res://scenes/maps/Outside_2/outside_2.tscn", "from_sams_cave")
+
+
 func configure_camera(cam: Camera2D) -> void:
 	# Cam Limits
 	cam.limit_left = -720
 	cam.limit_right = 750
 	cam.limit_top = -775
 	cam.limit_bottom = 200
-
-func no_flashlight():
-	pass
-	
