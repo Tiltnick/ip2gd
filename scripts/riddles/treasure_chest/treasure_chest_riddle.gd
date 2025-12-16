e0857cb2d922476f7af6ce4a3296db0bc47d75bd
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-12-16 02:04:58 +0100

    Zwischenstand

--- scripts/riddles/treasure_chest/treasure_chest_riddle.gd
+++ scripts/riddles/treasure_chest/treasure_chest_riddle.gd
@@ -8,12 +8,46 @@ extends CanvasLayer
 	$Control/Panel2/HBoxContainer/Input3
 ]
 
+signal code_verified(result: bool)
+var code_solved := false
 
 
-func _on_exit_pressed() -> void:
-	hide()
-	
+func _ready():
+	for input in inputs:
+		input.max_length = 1
+		input.virtual_keyboard_type = LineEdit.KEYBOARD_TYPE_NUMBER
+		input.text_changed.connect(_on_text_changed.bind(input))
+
+
+func _on_text_changed(new_text: String, input: LineEdit) -> void:
+	# Fokus weitergeben
+	if new_text.length() == 1:
+		var index := inputs.find(input)
+		if index != -1 and index < inputs.size() - 1:
+			inputs[index + 1].grab_focus()
+
+	# Nach jeder Eingabe prüfen
+	_try_verify_code()
+
+
+func _try_verify_code() -> void:
+	if code_solved:
+		return
+
+	# Prüfen ob alle Felder gefüllt sind
+	for input in inputs:
+		if input.text.length() != 1:
+			return
+
+	var entered: Array = []
+	for input in inputs:
+		entered.append(input.text)
+
+	if entered == code:
+		code_solved = true
+		GameState.puzzle_state["ship_exit_monologue_pending"] = true
+		emit_signal("code_verified", true)
+
 
 func _on_round_buttton_pressed() -> void:
 	hide()
-	
