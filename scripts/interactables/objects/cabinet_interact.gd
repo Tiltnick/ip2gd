a728870d23c345eb6b15e474bccbea272e6f138c
iancolaura <iancovesculaura@gmail.com>
2025-12-18 02:06:41 +0100

    zooming of telescope and fluxomat works

--- scripts/interactables/objects/cabinet_interact.gd
+++ scripts/interactables/objects/cabinet_interact.gd
@@ -1,37 +1,75 @@
-extends ZoomStoreItem
-class_name TelescopeItem
+extends Interactable
+class_name CabinetGiveTelescope
 
-func _ready() -> void:
-	save_id = "telescope1"
-	hotbar_id = "telescope"
+const SAVE_KEY := "telescope1"  #  Save-State Key
+const ITEM_ID  := "telescope"   # ItemDatabase Key / hotbar_id
 
-	item_name_de = "Teleskop"
-	item_name_en = "Telescope"
-	super._ready()
+@export var dialog_path := ""   #falls dialohue für cab
 
 func interact() -> void:
-	# In der Welt: direkt einsammeln, kein Zoom
-	if not spawned_from_hotbar:
-		_store_in_hotbar()
+	SfxPlayer.ui_click_sound()
+
+	# nur einmal geben
+	if GameState.puzzle_state.get(SAVE_KEY, false):
 		return
 
-	# Aus der Hotbar: normale Zoom-Logik
-	super.interact()
+	# Save-State setzen
+	GameState.puzzle_state[SAVE_KEY] = true
+
+	# Item in Hotbar + Inventory
+	hotbarglobal.add_item(ITEM_ID)
+
+	
+
+	# optionaler dialohe
+	if dialog_path != "":
+		DialogManager.start_dialog(dialog_path)
 
 
-	## Falls schon geöffnet gespeichert:
-	#if save_id != "" and GameState.puzzle_state.get(save_id, false):
-		#opened = true
-		#anim.frame = anim.sprite_frames.get_frame_count("open") - 1
-		#anim.stop()
 
+
+
+
+
+
+
+
+
+
+#extends ZoomStoreItem
+#class_name TelescopeItem
+#
+#func _ready() -> void:
+	#save_id = "telescope1"
+	#hotbar_id = "telescope"
+#
+	#item_name_de = "Teleskop"
+	#item_name_en = "Telescope"
+	#super._ready()
+#
 #func interact() -> void:
-	#if opened:
+	## In der Welt: direkt einsammeln, kein Zoom
+	#if not spawned_from_hotbar:
+		#_store_in_hotbar()
 		#return
 #
-	#opened = true
-	#if save_id != "":
-		#GameState.puzzle_state[save_id] = true
+	## Aus der Hotbar: normale Zoom-Logik
+	#super.interact()
 #
-	#anim.play("open")
-	
+#
+	### Falls schon geöffnet gespeichert:
+	##if save_id != "" and GameState.puzzle_state.get(save_id, false):
+		##opened = true
+		##anim.frame = anim.sprite_frames.get_frame_count("open") - 1
+		##anim.stop()
+#
+##func interact() -> void:
+	##if opened:
+		##return
+##
+	##opened = true
+	##if save_id != "":
+		##GameState.puzzle_state[save_id] = true
+##
+	##anim.play("open")
+	#
