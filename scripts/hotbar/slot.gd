9820b6be7934c515e23f1b10dc688690f5b8b58a
iancolaura <iancovesculaura@gmail.com>
2025-12-25 22:42:10 +0100

    final cleaned slot.gd

--- scripts/hotbar/slot.gd
+++ scripts/hotbar/slot.gd
@@ -5,26 +5,21 @@ class_name HotbarSlot
 @onready var key_label := $"Label for Keys"
 @onready var count_label := get_node_or_null("CountLabel") 
 
-#@export var show_shadow: bool = true
-#@export var show_key_label := true
 
 var slot_index := -1
 var click_callback: Callable = Callable()
 
 const SLOT_ICON_SIZE = Vector2(64, 64)
 
-#func _ready():
-	#if not show_shadow:
-		#_disable_shadow()
+func _ready():
 #
-	## Counter standardmäßig aus
-	#if count_label:
-		#count_label.visible = false
+	# Counter standardmäßig aus
+	if count_label:
+		count_label.visible = false
 
 
 func _gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
-		#print("Slot", slot_index, "wurrde angeklickt")
 		if click_callback:
 			click_callback.call(slot_index)
 
@@ -82,15 +77,6 @@ func set_stack_count(count: int) -> void:
 		count_label.visible = true
 	else:
 		count_label.visible = false
-
-
-#func _disable_shadow():
-	#var stylebox: StyleBox = $Background.get("theme_override_styles/panel")
-	#if stylebox is StyleBoxFlat:
-		#var new_style: StyleBoxFlat = stylebox.duplicate()
-		#new_style.shadow_size = 0
-		#new_style.shadow_color = Color(0,0,0,0)
-		#$Background.set("theme_override_styles/panel", new_style)
 
 
 func set_slot_index(i: int):
