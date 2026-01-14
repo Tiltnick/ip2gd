4e8d5d58d42024ef57dbcbe4df531d9baeba96ad
iancolaura <iancovesculaura@gmail.com>
2026-01-14 23:06:31 +0100

    first properly moving tumbleweed + script for simple npcs

--- scripts/state_machine_npc/simple movement/simple_movement_npc.gd
+++ scripts/state_machine_npc/simple movement/simple_movement_npc.gd
@@ -2,20 +2,32 @@ extends CharacterBody2D
 class_name AmbientPathNPC
 
 @export var path_follow: PathFollow2D
-@export var speed: float = 1.0
-@export var loop: bool = true
+@export var speed: float = 60.0 
+@export var ping_pong: bool = true
 
-func _physics_process(delta: float) -> void:
-	if path_follow == null:
-		return
+var _dir := 1.0
+var _len := 0.0
 
-	path_follow.progress += speed * delta
-
-	if loop:
+func _ready() -> void:
+	if path_follow:
 		var path := path_follow.get_parent() as Path2D
 		if path and path.curve:
-			var len := path.curve.get_baked_length()
-			if len > 0.0:
-				path_follow.progress = fposmod(path_follow.progress, len)
+			_len = path.curve.get_baked_length()
+
+func _physics_process(delta: float) -> void:
+	if path_follow == null or _len <= 0.0:
+		return
+
+	path_follow.progress += speed * _dir * delta
+
+	if ping_pong:
+		if path_follow.progress >= _len:
+			path_follow.progress = _len
+			_dir = -1.0
+		elif path_follow.progress <= 0.0:
+			path_follow.progress = 0.0
+			_dir = 1.0
+	else:
+		path_follow.progress = fposmod(path_follow.progress, _len)
 
 	global_position = path_follow.global_position
