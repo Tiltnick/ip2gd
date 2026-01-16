c1f8431b6721b9ebaa4363fda80bc46f0cb2be02
iancolaura <iancovesculaura@gmail.com>
2026-01-16 04:12:07 +0100

    added help function to hide e popup properly

--- scripts/interactables/interactable.gd
+++ scripts/interactables/interactable.gd
@@ -38,7 +38,7 @@ func _process(_delta):
 	if Input.is_action_just_pressed("interact"):
 		var allow := player_in_area
 
-		# wenn das Objekt "is_zoomed" hat und gerade gezoomt istdann erlauben, auch ohne player_in_area
+		# wenn das Objekt "is_zoomed" hat und gerade gezoomt ist
 		if not allow:
 			for p in get_property_list():
 				if p.name == "is_zoomed":
@@ -46,4 +46,19 @@ func _process(_delta):
 					break
 
 		if allow:
+			_hide_interact_ui()  
 			interact()
+
+
+func _hide_interact_ui() -> void:
+	player_in_area = false
+
+	if outline:
+		outline.visible = false
+
+	if e_popup_node:
+		e_popup_node.visible = false
+
+	if area:
+		
+		area.set_deferred("monitoring", false)
