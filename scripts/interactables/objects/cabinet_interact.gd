be0c643f6112dd9e66e6049019839c780ba14d2b
iancolaura <iancovesculaura@gmail.com>
2026-01-18 16:39:34 +0100

    re-added cabinet animation

--- scripts/interactables/objects/cabinet_interact.gd
+++ scripts/interactables/objects/cabinet_interact.gd
@@ -1,73 +1,55 @@
 extends Interactable
 class_name CabinetGiveTelescope
 
-const SAVE_KEY := "telescope1"  #  Save-State Key
-const ITEM_ID  := "telescope"   # ItemDatabase Key / hotbar_id
+const SAVE_KEY := "telescope1"
+const ITEM_ID  := "telescope"
 
-@export var dialog_path := "res://dialog/innerMonologue/discovering_telescope.json"   
+@export var dialog_path := "res://dialog/innerMonologue/discovering_telescope.json"
+
+@onready var anim: AnimatedSprite2D = $anim
+
+var opened := false
+
+
+func _ready() -> void:
+	super._ready()
+
+	# Lade gespeicherten Zustand
+	opened = bool(GameState.puzzle_state.get(SAVE_KEY, false))
+
+	if opened:
+		_set_open_visual()
+
 
 func interact() -> void:
 	SfxPlayer.ui_click_sound()
 
-	# nur einmal geben
-	if GameState.puzzle_state.get(SAVE_KEY, false):
+	# schon geöffnet? dann nix mehr tun
+	if opened:
 		return
 
-	# Save-State setzen
+	opened = true
 	GameState.puzzle_state[SAVE_KEY] = true
 
-	# Item in Hotbar + Inventory
+	# Visuell öffnen
+	_set_open_visual()
+
+	# Item geben
 	hotbarglobal.add_item(ITEM_ID)
 
-	
+	# Innerer Monolog
 	if dialog_path != "":
-		DialogManager.start_dialog(dialog_path)
+		call_deferred("_play_dialog")
 
 
+func _set_open_visual() -> void:
+	if anim.sprite_frames.has_animation("open"):
+		anim.play("open")
+	else:
+		# Fallback: letzter Frame
+		anim.frame = anim.sprite_frames.get_frame_count("open") - 1
+		anim.stop()
 
 
-
-
-
-
-
-
-
-
-#extends ZoomStoreItem
-#class_name TelescopeItem
-#
-#func _ready() -> void:
-	#save_id = "telescope1"
-	#hotbar_id = "telescope"
-#
-	#item_name_de = "Teleskop"
-	#item_name_en = "Telescope"
-	#super._ready()
-#
-#func interact() -> void:
-	## In der Welt: direkt einsammeln, kein Zoom
-	#if not spawned_from_hotbar:
-		#_store_in_hotbar()
-		#return
-#
-	## Aus der Hotbar: normale Zoom-Logik
-	#super.interact()
-#
-#
-	### Falls schon geöffnet gespeichert:
-	##if save_id != "" and GameState.puzzle_state.get(save_id, false):
-		##opened = true
-		##anim.frame = anim.sprite_frames.get_frame_count("open") - 1
-		##anim.stop()
-#
-##func interact() -> void:
-	##if opened:
-		##return
-##
-	##opened = true
-	##if save_id != "":
-		##GameState.puzzle_state[save_id] = true
-##
-	##anim.play("open")
-	#
+func _play_dialog() -> void:
+	DialogManager.start_dialog(dialog_path)
