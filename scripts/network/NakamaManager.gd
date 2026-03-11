360fdc7eb2ff5a92d0ecc085b9403ec67095bacf
iancolaura <iancovesculaura@gmail.com>
2026-03-11 16:05:30 +0100

    create + read Post function done

--- scripts/network/NakamaManager.gd
+++ scripts/network/NakamaManager.gd
@@ -10,6 +10,7 @@ func _ready():
 
 	login_device()
 
+
 func login_device():
 	var device_id = OS.get_unique_id()
 	print("Device ID: ", device_id)
@@ -17,8 +18,72 @@ func login_device():
 	var result = await client.authenticate_device_async(device_id)
 
 	if result.is_exception():
-		print("Login issue: ", result)
+		print("Login Error: ", result)
 		return
 
 	session = result
-	print("Successful login: ", session.user_id)
+	print("Successful Login: User ID: ", session.user_id)
+
+	await save_test_post()
+	await load_test_post()
+
+
+func save_test_post():
+	var post_data = {
+		"author": "Sam",
+		"caption": "Test Post 1",
+		"likes": 5
+	}
+
+	var can_read = 2
+	var can_write = 1
+	var version = ""
+
+	var write = NakamaWriteStorageObject.new(
+		"spacegram_posts",
+		"post_1",
+		can_read,
+		can_write,
+		JSON.stringify(post_data),
+		version
+	)
+
+	var acks = await client.write_storage_objects_async(session, [write])
+
+	if acks.is_exception():
+		print("Saving failed: ", acks)
+		return
+
+	print("Post saved successfully: ", acks)
+
+
+func load_test_post():
+	var object_ids = [
+		NakamaStorageObjectId.new("spacegram_posts", "post_1", session.user_id)
+	]
+
+	var result = await client.read_storage_objects_async(session, object_ids)
+
+	if result.is_exception():
+		print("Reading failed: ", result)
+		return
+
+	print("Read objects: ", result.objects)
+
+	if result.objects.size() == 0:
+		print("Post not found.")
+		return
+
+	var raw_value = result.objects[0].value
+	var json = JSON.new()
+	var parse_result = json.parse(raw_value)
+
+	if parse_result != OK:
+		print("JSON Parse Error")
+		return
+
+	var post_data = json.data
+	print("Loaded Post:")
+	print("Author: ", post_data["author"])
+	print("Caption: ", post_data["caption"])
+	print("Likes: ", post_data["likes"])
