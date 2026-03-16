05cc0437dc7b2356dd44e62f6c743d1327ce4dbd
iancolaura <iancovesculaura@gmail.com>
2026-03-16 15:55:32 +0100

    tried all new tables

--- BloomOfMemoryDB.session.sql
+++ BloomOfMemoryDB.session.sql
@@ -1,23 +1,15 @@
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'public';
-
-INSERT INTO profile (user_id, bio, profile_picture, display_name)
+insert into comment(comment_id, post_id, user_id, text)
 VALUES (
-    '11111111-1111-1111-1111-111111111111',
-    'Test bio',
-    'assets/profile.png',
-    'Oris'
-);
-
-SELECT * FROM profile
-
-INSERT INTO post (post_id, user_id, caption, image_path)
-VALUES (
+    '33333333-3333-3333-3333-333333333333',
     '22222222-2222-2222-2222-222222222222',
     '11111111-1111-1111-1111-111111111111',
-    'Mein erster Post',
-    'assets/posts/test.png'
+    'Nice post!'
 );
 
-SELECT * FROM post;
+SELECT * from comment; 
+
+
+SELECT COUNT(*)
+FROM likes
+WHERE post_id = '22222222-2222-2222-2222-222222222222';
+
