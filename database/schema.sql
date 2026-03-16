25acfca25774bdeee11c41537c943ba68a62c75a
iancolaura <iancovesculaura@gmail.com>
2026-03-16 13:48:13 +0100

    updated schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,19 +1,13 @@
-CREATE TABLE "user" (
-    user_id UUID PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    email TEXT UNIQUE NOT NULL
-);
-
 CREATE TABLE profile (
-    user_id UUID PRIMARY KEY REFERENCES "user"(user_id),
+    user_id UUID PRIMARY KEY,
     bio TEXT,
     profile_picture TEXT,
-    display_name TEXT 
+    display_name TEXT
 );
 
 CREATE TABLE post (
     post_id UUID PRIMARY KEY,
-    user_id UUID REFERENCES "user"(user_id),
+    user_id UUID NOT NULL,
     caption TEXT,
     image_path TEXT,
     posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -21,21 +15,14 @@ CREATE TABLE post (
 
 CREATE TABLE comment (
     comment_id UUID PRIMARY KEY,
-    post_id UUID REFERENCES post(post_id),
-    user_id UUID REFERENCES "user"(user_id),
+    post_id UUID NOT NULL REFERENCES post(post_id),
+    user_id UUID NOT NULL,
     text TEXT,
     posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE follow (
-    follower_user_id UUID REFERENCES "user"(user_id),
-    followed_user_id UUID REFERENCES "user"(user_id),
-    status TEXT,
-    PRIMARY KEY (follower_user_id, followed_user_id)
-);
-
 CREATE TABLE likes (
-    user_id UUID REFERENCES "user"(user_id),
-    post_id UUID REFERENCES post(post_id),
+    user_id UUID NOT NULL,
+    post_id UUID NOT NULL REFERENCES post(post_id),
     PRIMARY KEY (user_id, post_id)
 );
