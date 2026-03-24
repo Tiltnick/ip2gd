81540428760a2a1c023a7ee8d768b686447fb437
iancolaura <iancovesculaura@gmail.com>
2026-03-24 11:32:37 +0100

    added login and register per email and logout

--- scripts/network/NakamaManager.gd
+++ scripts/network/NakamaManager.gd
@@ -29,6 +29,42 @@ func login_device():
 	session = result
 	print("Successful Login: User ID: ", session.user_id)
 
+func register_email(email: String, password: String, username: String = ""):
+	if client == null:
+		print("Client nicht initialisiert.")
+		return
+
+	var result = await client.authenticate_email_async(email, password, username, true)
+
+	if result.is_exception():
+		print("Register Error: ", result)
+		return
+
+	session = result
+	print("Registration successful. User ID: ", session.user_id)
+
+func login_email(email: String, password: String):
+
+	if client == null:
+		print("Client nicht initialisiert.")
+		return
+
+	var result = await client.authenticate_email_async(email, password, "", false)
+
+	if result.is_exception():
+		print("Login Error: ", result)
+		return
+
+	session = result
+	print("Login successful. User ID: ", session.user_id)
+
+func logout():
+	if session == null:
+		return
+
+	var result = await client.session_logout_async(session)
+	session = null
+	print("Logged out.")
 
 func is_logged_in() -> bool:
 	return session != null
